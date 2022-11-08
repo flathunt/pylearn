3c12c9651f04df098de65225f68f537c36253d33
Marcus <porkpie@gmail.com>
2022-11-08 20:02:44 +0000

    kjfdhg

--- tube_status.sh
+++ tube_status.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo setterm --term linux --blank=1 < /dev/tty1
-trap "sudo setterm --term linux --cursor on < /dev/tty1 ; exit" 2
+setterm --term linux --blank=1 < /dev/tty1
+trap "setterm --term linux --cursor on < /dev/tty1 ; exit" 2
 
 curl https://api.tfl.gov.uk/Line/district,northern,piccadilly,central/Disruption 2> /dev/null | jq -r '.[] | .description' > /tmp/tubestatus.out
 
 if [ -s /tmp/tubestatus.out ]
 then
-  sudo setterm --term linux --blank=poke < /dev/tty1
-  sudo setterm --term linux --cursor off < /dev/tty1
+  setterm --term linux --blank=poke < /dev/tty1
+  setterm --term linux --cursor off < /dev/tty1
   printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" > /dev/tty1
   cat /tmp/tubestatus.out | while read line rest
   do
@@ -19,10 +19,10 @@ then
       District|district)	fg=white; bg=green;;
       *)			fg=white; bg=black;;
     esac
-    sudo setterm --term linux --background=$bg --foreground=$fg  > /dev/tty1
+    setterm --term linux --background=$bg --foreground=$fg  > /dev/tty1
     echo "" > /dev/tty1
     echo "$line $rest" > /dev/tty1
-    sudo setterm --term linux --background=black --foreground=white > /dev/tty1
+    setterm --term linux --background=black --foreground=white > /dev/tty1
   done
   i=1
   while [ "$i" -lt "15" ]
@@ -32,4 +32,4 @@ then
     i=$((i+1))
   done
 fi
-sudo setterm --term linux --cursor on < /dev/tty1
+setterm --term linux --cursor on < /dev/tty1
